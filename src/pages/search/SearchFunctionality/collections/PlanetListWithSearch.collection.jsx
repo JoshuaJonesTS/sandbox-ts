8b3e81e8e567a9322a1b276ef61209c9cb1a374d
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-20 13:45:07 -0800

    Add planets

--- src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
+++ src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
@@ -35,7 +35,31 @@ const samplePlanetData = [
     {
         id: 2,
         name: "Venus"
-    }
+    },
+    {
+        id: 3,
+        name: "Earth"
+    },
+    {
+        id: 4,
+        name: "Mars"
+    },
+    {
+        id: 5,
+        name: "Jupiter"
+    },
+    {
+        id: 6,
+        name: "Saturn"
+    },
+    {
+        id: 7,
+        name: "Uranus"
+    },
+    {
+        id: 8,
+        name: "Neptune"
+    },
 ];
 
 const PlanetListWithSearch = (data = { allPlanets: [...samplePlanetData] }, loading, error) => {
