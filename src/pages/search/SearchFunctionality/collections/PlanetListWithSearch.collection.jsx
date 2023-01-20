88027c769f8f550b4b8cb6575626ae8a5ed501b2
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-20 14:39:45 -0800

    Fix sample data error

--- src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
+++ src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
@@ -33,6 +33,14 @@ const PlanetColumnConfigs = [
         objectFieldsDelimiter: "",
         width: 100,
         useNestedObject: false
+    },
+    {
+        name: "moon",
+        title: "Moon",
+        objectFieldsToDisplay: ["largestMoon.name"],
+        objectFieldsDelimiter: "",
+        width: 100,
+        useNestedObject: true
     }
 ];
 
@@ -41,108 +49,111 @@ const samplePlanetData = [
         id: 1,
         name: "Mercury",
         type: "rocky",
-        largestMoon: null
+        largestMoon: {
+            name: null,
+            size: [
+                {
+                    diameter: null,
+                    mass: null
+                }
+            ],
+        }
     },
     {
         id: 2,
         name: "Venus",
         type: "rocky",
-        largestMoon: null
+        largestMoon: {
+            name: null,
+            size: [
+                {
+                    diameter: null,
+                    mass: null
+                }
+            ],
+        }
     },
     {
         id: 3,
         name: "Earth",
         type: "rocky",
-        largestMoon: [
-            {
-                name: "Moon",
-                size: [
-                    {
-                        diameter: 2159.1, // miles
-                        mass: 7.35e22 // kg
-                    }
-                ],
-            }
-        ]
+        largestMoon: {
+            name: "Moon",
+            size: [
+                {
+                    diameter: 2159.1, // miles
+                    mass: 7.35e22 // kg
+                }
+            ],
+        }
     },
     {
         id: 4,
         name: "Mars",
         type: "rocky",
-        largestMoon: [
-            {
-                name: "Phobos",
-                size: {
-                    diameter: 14.002,
-                    mass: 10.6e15
-                }
+        largestMoon: {
+            name: "Phobos",
+            size: {
+                diameter: 14.002,
+                mass: 10.6e15
             }
-        ]
+        }
     },
     {
         id: 5,
         name: "Jupiter",
         type: "gas",
-        largestMoon: [
-            {
-                name: "Ganymede",
-                size: [
-                    {
-                        diameter: 3273.5,
-                        mass: 1.48e23
-                    }
-                ]
-            }
-
-        ]
+        largestMoon: {
+            name: "Ganymede",
+            size: [
+                {
+                    diameter: 3273.5,
+                    mass: 1.48e23
+                }
+            ]
+        }
     },
     {
         id: 6,
         name: "Saturn",
         type: "gas",
-        largestMoon: [
-            {
-                name: "Titan",
-                size: [
-                    {
-                        diameter: 3199.7,
-                        mass: 1.34e23
-                    }
-                ]
-            }
-        ]
+        largestMoon: {
+            name: "Titan",
+            size: [
+                {
+                    diameter: 3199.7,
+                    mass: 1.34e23
+                }
+            ]
+        }
     },
     {
         id: 7,
         name: "Uranus",
         type: "gas",
-        largestMoon: [
-            {
-                name: "Titania",
-                size: [
-                    {
-                        diameter: 970.78,
-                        mass: 3.4e21
-                    }
-                ]
-            }
-        ]
+        largestMoon: {
+            name: "Titania",
+            size: [
+                {
+                    diameter: 970.78,
+                    mass: 3.4e21
+                }
+            ]
+        }
     },
     {
         id: 8,
         name: "Neptune",
         type: "gas",
-        largestMoon: [
-            {
-                name: "Triton",
-                size: [
-                    {
-                        diameter: 1681.9,
-                        mass: 2.14e22
-                    }
-                ]
-            }
-        ]
+        largestMoon: {
+            name: "Triton",
+            size: [
+                {
+                    diameter: 1681.9,
+                    mass: 2.14e22
+                }
+            ]
+        }
     },
 ];
 
