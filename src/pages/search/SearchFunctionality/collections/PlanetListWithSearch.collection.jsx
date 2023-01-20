578690bf2bd6a565bb7a2c6b845e0c67c3274aca
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-20 13:20:25 -0800

    Add key
    Add the key where the data is coming from and use it in the TableWithSearch

--- src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
+++ src/pages/search/SearchFunctionality/collections/PlanetListWithSearch.collection.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 // block groups
 import TableWithSearch from '../../../../blockGroups/table/TableWithSearch/TableWithSearch.blockgroup';
 
-const PlanetListWithSearch = (data = {}, loading, error) => {
+const PlanetListWithSearch = (data = { allPlanets: [] }, loading, error) => {
     return(
         <TableWithSearch
-            rawData={}
+            rawData={data.allPlanets}
             loading={loading}
             error={error}
             fieldOptions={}
